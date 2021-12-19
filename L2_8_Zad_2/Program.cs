f6d0e737a593ace561ddc374daaca6e19cb84285
Damian Z <damian9203@gmail.com>
2021-12-19 12:18:58 +0100

    Add project files.

---
+++ L2_8_Zad_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace L2_8_Zad_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startNumber = 0;
+            int endNumber = 1000;
+
+            do
+            {
+                if (startNumber % 2 == 0)
+                {
+                    Console.WriteLine("Liczba parzysta: " + startNumber);
+                }
+                startNumber++;
+            } while (startNumber < endNumber);
+        }
+    }
+}
